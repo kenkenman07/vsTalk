84c635ff842d006e07a60958ef2746f1cc79cc81
kenkenman07 <ken0522g@icloud.com>
2026-01-28 12:47:47 +0900

    デザイン

--- frontend/src/pages/Meeting.tsx
+++ frontend/src/pages/Meeting.tsx
@@ -36,57 +36,81 @@ function Meeting() {
             {message && <ResultModal message={message} onClose={reload}/>}
                 
             <div className="fixed inset-0 flex flex-col items-center justify-center">
-                <Timer />
 
-                <label>
-                    話の繰り返しが起きています
-                </label>
+                {!message && <Timer />}
+
+                <div>
                     <input 
                     type="radio"
+                    id="radio-input1"
                     name="reason"
                     value="話の繰り返しが起きています"
-                    onChange={(e) => setReason(e.target.value)}
+                    onChange={(e) => {setReason(e.target.value)}}
+                    checked={reason === '話の繰り返しが起きています'}
+                    className="peer hidden"
                     />
+                    <label htmlFor="radio-input1" className="relative mx-0 my-1 block w-90 cursor-pointer rounded-lg border-2 border-solid border-[#B9B9B9] px-12 py-2.5 text-sm delay-0 duration-500 ease-in-out before:invisible before:absolute before:left-[19px] before:top-2 before:h-5 before:w-5 before:scale-[3] before:rounded-full before:bg-black before:opacity-0 before:delay-0 before:duration-500 before:ease-in-out before:content-[''] after:absolute after:left-[21px] after:top-3 after:h-4 after:w-4 after:rounded-full after:border-2 after:border-solid after:border-black after:content-[''] peer-checked:border-black peer-checked:before:visible peer-checked:before:scale-100 peer-checked:before:opacity-100">
+                        話の繰り返しが起きています
+                    </label>
+                </div>
 
-                <label>
-                    話が脱線しています
-                </label>
+                <div>
                     <input 
                     type="radio"
+                    id="radio-input2"
                     name="reason"
                     value="話が脱線しています"
-                    onChange={(e) => setReason(e.target.value)}
+                    onChange={(e) => {setReason(e.target.value)}}
+                    checked={reason === '話が脱線しています'}
+                    className="peer hidden"
                     />
+                    <label htmlFor="radio-input2" className="relative mx-0 my-1 block w-90 cursor-pointer rounded-lg border-2 border-solid border-[#B9B9B9] px-12 py-2.5 text-sm delay-0 duration-500 ease-in-out before:invisible before:absolute before:left-[19px] before:top-2 before:h-5 before:w-5 before:scale-[3] before:rounded-full before:bg-black before:opacity-0 before:delay-0 before:duration-500 before:ease-in-out before:content-[''] after:absolute after:left-[21px] after:top-3 after:h-4 after:w-4 after:rounded-full after:border-2 after:border-solid after:border-black after:content-[''] peer-checked:border-black peer-checked:before:visible peer-checked:before:scale-100 peer-checked:before:opacity-100">
+                        話が脱線しています
+                    </label>
+                </div>
 
-                <label>
-                    前の議事録を再確認してください
-                </label>
+                <div>
+
                     <input 
                     type="radio"
+                    id="radio-input3"
                     name="reason"
                     value="前の議事録を再確認してください"
-                    onChange={(e) => setReason(e.target.value)}
+                    onChange={(e) => {setReason(e.target.value)}}
+                    checked={reason === '前の議事録を再確認してください'}
+                    className="peer hidden"
                     />
-                
-                <label>
-                    その他
-                </label>
+                    <label htmlFor="radio-input3" className="relative mx-0 my-1 block w-90 cursor-pointer rounded-lg border-2 border-solid border-[#B9B9B9] px-12 py-2.5 text-sm delay-0 duration-500 ease-in-out before:invisible before:absolute before:left-[19px] before:top-2 before:h-5 before:w-5 before:scale-[3] before:rounded-full before:bg-black before:opacity-0 before:delay-0 before:duration-500 before:ease-in-out before:content-[''] after:absolute after:left-[21px] after:top-3 after:h-4 after:w-4 after:rounded-full after:border-2 after:border-solid after:border-black after:content-[''] peer-checked:border-black peer-checked:before:visible peer-checked:before:scale-100 peer-checked:before:opacity-100">
+                        前の議事録を再確認してください
+                    </label>
+                </div>
+
+                <div>
+
                     <input 
+                    type="radio"
+                    id="radio-input4"
                     name="reason"
-                    placeholder="自由記述"
-                    onChange={(e) => setReason(e.target.value)}
+                    value="話が長すぎます"
+                    onChange={(e) => {setReason(e.target.value)}}
+                    checked={reason === '話が長すぎます'}
+                    className="peer hidden"
                     />
+                    <label htmlFor="radio-input4" className="relative mx-0 my-1 block w-90 cursor-pointer rounded-lg border-2 border-solid border-[#B9B9B9] px-12 py-2.5 text-sm delay-0 duration-500 ease-in-out before:invisible before:absolute before:left-[19px] before:top-2 before:h-5 before:w-5 before:scale-[3] before:rounded-full before:bg-black before:opacity-0 before:delay-0 before:duration-500 before:ease-in-out before:content-[''] after:absolute after:left-[21px] after:top-3 after:h-4 after:w-4 after:rounded-full after:border-2 after:border-solid after:border-black after:content-[''] peer-checked:border-black peer-checked:before:visible peer-checked:before:scale-100 peer-checked:before:opacity-100">
+                        話が長すぎます
+                    </label>
+                </div>
+                
                 <div>
                     <button
                         disabled={!reason}
                         onClick={stop}
+                        className="w-24 h-24 rounded-full font-bold bg-red-600 hover:bg-red-700"
                         >
                         ボタン
                     </button>
                 </div>
 
-                <div>Message: {message}</div>
-            
             </div>
         </div>        
     )
