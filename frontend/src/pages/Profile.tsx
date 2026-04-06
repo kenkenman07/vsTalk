1c15a0a722e2287b8a627f2e08b0fcc78855c55d
kenkenman07 <ken0522g@icloud.com>
2026-04-06 16:45:47 +0900

    プロフィール画面実装途中

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -16,13 +16,25 @@ export default function Profile() {
 
   return (
     <div>
-      <button
-        onClick={handleSignOut}
-        className="inline-flex items-center gap-2 text-gray-300 hover:text-white transition-colors group"
-      >
-        <LogOut className="w-5 h-5 group-hover:-translate-x-1 transition-transform" />
-        <span>ログアウト</span>
-      </button>
+      <div className="flex min-h-screen items-center justify-center bg-gray-200">
+        <div className="flex flex-col items-center gap-26 bg-white p-25 rounded-lg shadow-md">
+          <h1 className="text-3xl font-bold">プロフィール</h1>
+
+          <div className="flex flex-col gap-4 bg-red-200 px-15 py-6 rounded-lg">
+            <div className="text-xl font-medium">増田健斗</div>
+            <div>メアド</div>
+          </div>
+        </div>
+        <div className="fixed right-10 bottom-10">
+          <button
+            onClick={handleSignOut}
+            className="inline-flex items-center gap-2 text-blue-600 hover:text-black transition-colors group"
+          >
+            <LogOut className="w-5 h-5 group-hover:-translate-x-1 transition-transform" />
+            <span>ログアウト</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
