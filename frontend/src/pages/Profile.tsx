7a659a85162f662dc673d916c7334a3175dd8c27
kenkenman07 <ken0522g@icloud.com>
2026-04-07 09:02:16 +0900

    プロフィール実装

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,11 +1,10 @@
 import { useCurrentUserStore } from "../modules/auth/current-user.state";
 import { LogOut } from "lucide-react";
 import { authRepository } from "../modules/auth/auth.repository";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Profile() {
   const currentUserStore = useCurrentUserStore();
-
   const navigate = useNavigate();
 
   const handleSignOut = async () => {
@@ -14,15 +13,24 @@ export default function Profile() {
     navigate("/signin");
   };
 
+  if (!currentUserStore) return;
+  const user = currentUserStore.currentUser;
+
   return (
     <div>
       <div className="flex min-h-screen items-center justify-center bg-gray-200">
+        <Link to="/" className="fixed left-20 top-10">
+          戻るボタン
+        </Link>
+
         <div className="flex flex-col items-center gap-26 bg-white p-25 rounded-lg shadow-md">
           <h1 className="text-3xl font-bold">プロフィール</h1>
 
-          <div className="flex flex-col gap-4 bg-red-200 px-15 py-6 rounded-lg">
-            <div className="text-xl font-medium">増田健斗</div>
-            <div>メアド</div>
+          <div className="flex flex-col gap-4 bg-red-300 px-15 py-6 rounded-lg">
+            <div className="text-xl font-medium">
+              {user?.user_metadata.name}
+            </div>
+            <div>{user?.email}</div>
           </div>
         </div>
         <div className="fixed right-10 bottom-10">
