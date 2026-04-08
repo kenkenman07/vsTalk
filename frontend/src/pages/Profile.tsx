d63b63e1b157fa2a6f812a87c812af89a3abca43
kenkenman07 <ken0522g@icloud.com>
2026-04-08 13:34:27 +0900

    Profileレスポンシブ実装

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -17,13 +17,13 @@ export default function Profile() {
   const user = currentUserStore.currentUser;
 
   return (
-    <div>
-      <div className="flex min-h-screen items-center justify-center bg-gray-200">
-        <Link to="/" className="fixed left-20 top-10">
-          戻るボタン
-        </Link>
+    <div className="min-h-screen bg-gray-100 px-5 py-7 sm:px-6">
+      <Link to="/" className="fixed left-20 top-10">
+        戻るボタン
+      </Link>
 
-        <div className="flex flex-col items-center gap-26 bg-white p-25 rounded-lg shadow-md">
+      <div className="flex min-h-screen items-center justify-center">
+        <div className="w-full max-w-sm flex flex-col items-center gap-20 bg-white p-4 sm:py-12 rounded-lg shadow-md">
           <h1 className="text-3xl font-bold">プロフィール</h1>
 
           <div className="flex flex-col gap-4 bg-red-300 px-15 py-6 rounded-lg">
