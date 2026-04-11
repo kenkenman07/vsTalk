bf81791d7b04a3c8685898145dc5463f59fd590d
kenkenman07 <ken0522g@icloud.com>
2026-04-11 15:26:34 +0900

    Profileページ名前変更UI実装

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,10 +1,14 @@
 import { useCurrentUserStore } from "../modules/auth/current-user.state";
-import { LogOut } from "lucide-react";
+import { LogOut, Pencil, X } from "lucide-react";
 import { authRepository } from "../modules/auth/auth.repository";
 import { useNavigate } from "react-router-dom";
 import ReturnButton from "../components/ReturnButton";
+import { useState } from "react";
 
 export default function Profile() {
+  const [open, setOpen] = useState(false);
+  const [newName, setNewName] = useState("");
+
   const currentUserStore = useCurrentUserStore();
   const navigate = useNavigate();
 
@@ -13,6 +17,8 @@ export default function Profile() {
     currentUserStore.set(undefined);
     navigate("/signin");
   };
+
+  const handleChangeName = async () => {};
 
   if (!currentUserStore) return;
   const user = currentUserStore.currentUser;
@@ -25,11 +31,42 @@ export default function Profile() {
         <div className="w-full max-w-sm flex flex-col items-center gap-20 bg-white p-4 sm:py-12 rounded-lg shadow-md">
           <h1 className="text-3xl font-bold">プロフィール</h1>
 
-          <div className="flex flex-col gap-4 bg-red-300 px-15 py-6 rounded-lg">
-            <div className="text-xl font-medium">
-              {user?.user_metadata.name}
-            </div>
-            <div>{user?.email}</div>
+          <div className="w-full max-w-md h-32  bg-red-300 px-15 py-6 rounded-lg">
+            {open ? (
+              <div className="flex flex-col gap-6 items-center">
+                <div className="flex h-7 gap-2 items-center font-medium">
+                  <input
+                    type="text"
+                    placeholder="新しい名前"
+                    onChange={(e) => setNewName(e.target.value)}
+                    className="border rounded-lg  px-1 py-2"
+                  />
+                  <button
+                    onClick={() => setOpen(false)}
+                    className="border rounded-full"
+                  >
+                    <X size={22} />
+                  </button>
+                </div>
+
+                <button
+                  onClick={handleChangeName}
+                  className="rounded-xl px-3 py-1 bg-black text-white"
+                >
+                  変更
+                </button>
+              </div>
+            ) : (
+              <div className="flex flex-col gap-5">
+                <div className="flex gap-2 items-center text-xl font-medium">
+                  {user?.user_metadata.name}
+                  <button onClick={() => setOpen(true)}>
+                    <Pencil size={22} />
+                  </button>
+                </div>
+                <div>{user?.email}</div>
+              </div>
+            )}
           </div>
         </div>
         <div className="fixed right-10 bottom-10">
